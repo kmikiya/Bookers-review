858c1e77dd488930f4c28e34c49a01ecf98e6673
kmikiya <k.mikiya97@gmail.com>
2021-11-03 10:47:31 +0000

    cfwc

---
+++ app/models/book.rb
@@ -0,0 +1,14 @@
+class Book < ApplicationRecord
+  belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+  validates :evaluation, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1}, presence: true
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+end
